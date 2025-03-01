bc6319465072ee0d64b7d116adcf72cde11af3f5
treetree226 <tthai2025@kehillahstudent.org>
2025-02-28 23:53:31 -0800

    done

--- q1.py
+++ q1.py
@@ -4,12 +4,20 @@
 # 7, 9, "a", "cat", False
 # Assign this list to the variable my_list. Then print my_list
 
-#YOUR CODE HERE
+my_list= ["7", "9", "a", "cat", "False"]
+print (my_list)
+
 
 # b) Append 3.14 and 7 to the list. Then print my_list
 
 #YOUR CODE HERE
+my_list.append(["3.14","7"])
+print(my_list)
 
 # c) Find and print the index of "cat"
 
 #YOUR CODE HERE
+if "cat" in my_list:
+    my_list.index("cat")
+
+
